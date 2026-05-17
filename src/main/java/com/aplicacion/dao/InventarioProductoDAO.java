d36d5fe74f60c5c6f7bc1b77fcd5ed83521a2dfc
pablo <proldanv2@miumg.edu.gt>
2026-05-16 19:31:20 -0600

    cambios

--- src/main/java/com/aplicacion/dao/InventarioProductoDAO.java
+++ src/main/java/com/aplicacion/dao/InventarioProductoDAO.java
@@ -152,27 +152,43 @@ public class InventarioProductoDAO {
 
         Connection con = null;
         PreparedStatement ps = null;
+        ResultSet rs = null;
 
         String sql = "SELECT * FROM Inventario_producto WHERE id_inventario = ?";
 
         try {
-
             con = ConexionBD.getConexion();
-
             ps = con.prepareStatement(sql);
 
-            // ID obligatorio
             ps.setInt(1, id);
 
+            rs = ps.executeQuery();
 
+            while(rs.next()) {
+
+                System.out.println("----------------------------");
+
+                System.out.println("ID: "
+                        + rs.getInt("id_inventario"));
+
+                System.out.println("ID de Producto: "
+                        + rs.getInt("id_producto"));
+
+                System.out.println("ID de almacen: "
+                        + rs.getInt("id_almacen"));
+
+                System.out.println("Stock: "
+                        + rs.getInt("stock_actual"));
+            }
 
         } catch (Exception e) {
 
-            System.out.println("Error al buscar producto");
+            System.out.println("Error al buscar el producto");
             e.printStackTrace();
 
         } finally {
 
+            try { if(rs != null) rs.close(); } catch(Exception e){}
             try { if(ps != null) ps.close(); } catch(Exception e){}
             try { if(con != null) con.close(); } catch(Exception e){}
         }
